3b97778dd629d8553783a606d7ddcd6f3057a425
Catarina Bressan <catarina@catarinapbressan.org>
2026-04-07 18:32:01 -0300

    Moves fastapi app to module level

--- allocations/entrypoints/fastapi_app.py
+++ allocations/entrypoints/fastapi_app.py
@@ -63,65 +63,60 @@ def session_maker():
     return sqlalchemy_orm.sessionmaker(engine)
 
 
-def init_app():
-    app = FastAPI()
-
-    @app.post(
-        "/allocate",
-        status_code=status.HTTP_201_CREATED,
-        response_model=BatchReference,
-        responses={status.HTTP_400_BAD_REQUEST: {"model": Message}},
-    )
-    def allocate(
-        session_maker: Annotated[
-            sqlalchemy_orm.sessionmaker[sqlalchemy_orm.Session], Depends(session_maker)
-        ],
-        order_line: OrderLine,
-    ) -> Any:
-        uow = unit_of_work.SQLAlchemyProductUnitOfWork(session_maker)
-        try:
-            reference = services.allocate(
-                order_line.order_id, order_line.sku, order_line.quantity, uow
-            )
-        except (exceptions.OutOfStockError, services.InvalidSKUError) as e:
-            return JSONResponse({"message": str(e)}, status.HTTP_400_BAD_REQUEST)
-
-        return BatchReference(reference=reference)
-
-    @app.post(
-        "/deallocate",
-        status_code=status.HTTP_200_OK,
-        response_model=BatchReference,
-        responses={status.HTTP_400_BAD_REQUEST: {"model": Message}},
-    )
-    def deallocate(
-        session_maker: Annotated[
-            sqlalchemy_orm.sessionmaker[sqlalchemy_orm.Session], Depends(session_maker)
-        ],
-        order_line: OrderLine,
-    ) -> Any:
-        uow = unit_of_work.SQLAlchemyProductUnitOfWork(session_maker)
-        try:
-            reference = services.deallocate(
-                order_line.order_id, order_line.sku, order_line.quantity, uow
-            )
-        except (exceptions.OutOfStockError, services.InvalidSKUError) as e:
-            return JSONResponse({"message": str(e)}, status.HTTP_400_BAD_REQUEST)
-
-        return BatchReference(reference=reference)
-
-    @app.post("/add_batch", status_code=status.HTTP_201_CREATED)
-    def add_batch(
-        session_maker: Annotated[
-            sqlalchemy_orm.sessionmaker[sqlalchemy_orm.Session], Depends(session_maker)
-        ],
-        batch: BatchIn,
-    ) -> BatchOut:
-        uow = unit_of_work.SQLAlchemyProductUnitOfWork(session_maker)
-        _batch = services.add_batch(batch.reference, batch.sku, batch.quantity, batch.eta, uow)
-        return BatchOut.from_domain(_batch)
-
-    return app
+app = FastAPI()
 
 
-app = init_app()
+@app.post(
+    "/allocate",
+    status_code=status.HTTP_201_CREATED,
+    response_model=BatchReference,
+    responses={status.HTTP_400_BAD_REQUEST: {"model": Message}},
+)
+def allocate(
+    session_maker: Annotated[
+        sqlalchemy_orm.sessionmaker[sqlalchemy_orm.Session], Depends(session_maker)
+    ],
+    order_line: OrderLine,
+) -> Any:
+    uow = unit_of_work.SQLAlchemyProductUnitOfWork(session_maker)
+    try:
+        reference = services.allocate(order_line.order_id, order_line.sku, order_line.quantity, uow)
+    except (exceptions.OutOfStockError, services.InvalidSKUError) as e:
+        return JSONResponse({"message": str(e)}, status.HTTP_400_BAD_REQUEST)
+
+    return BatchReference(reference=reference)
+
+
+@app.post(
+    "/deallocate",
+    status_code=status.HTTP_200_OK,
+    response_model=BatchReference,
+    responses={status.HTTP_400_BAD_REQUEST: {"model": Message}},
+)
+def deallocate(
+    session_maker: Annotated[
+        sqlalchemy_orm.sessionmaker[sqlalchemy_orm.Session], Depends(session_maker)
+    ],
+    order_line: OrderLine,
+) -> Any:
+    uow = unit_of_work.SQLAlchemyProductUnitOfWork(session_maker)
+    try:
+        reference = services.deallocate(
+            order_line.order_id, order_line.sku, order_line.quantity, uow
+        )
+    except (exceptions.OutOfStockError, services.InvalidSKUError) as e:
+        return JSONResponse({"message": str(e)}, status.HTTP_400_BAD_REQUEST)
+
+    return BatchReference(reference=reference)
+
+
+@app.post("/add_batch", status_code=status.HTTP_201_CREATED)
+def add_batch(
+    session_maker: Annotated[
+        sqlalchemy_orm.sessionmaker[sqlalchemy_orm.Session], Depends(session_maker)
+    ],
+    batch: BatchIn,
+) -> BatchOut:
+    uow = unit_of_work.SQLAlchemyProductUnitOfWork(session_maker)
+    _batch = services.add_batch(batch.reference, batch.sku, batch.quantity, batch.eta, uow)
+    return BatchOut.from_domain(_batch)
