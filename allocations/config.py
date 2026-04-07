10cff5ecd344f5ec001995fa30a03207402cff13
Catarina Bressan <catarina@catarinapbressan.org>
2026-04-06 21:49:18 -0300

    Uses test DB for testing
    Also deletes old SQLite stuff

--- allocations/config.py
+++ allocations/config.py
@@ -12,9 +12,14 @@ def get_app_sqlite() -> str:
     return get_sqlite(os.environ["SQLITE_NAME"])
 
 
-def get_postgres() -> str:
+def get_postgres(test=False) -> str:
+    db_name = os.environ["POSTGRES_DB"]
+
+    if test:
+        db_name = f"{db_name}-test"
+
     return (
         "postgresql+psycopg"
         f"://{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_PASSWORD"]}"
-        f"@{os.environ["POSTGRES_HOST"]}:{os.environ["POSTGRES_PORT"]}/{os.environ["POSTGRES_DB"]}"
+        f"@{os.environ["POSTGRES_HOST"]}:{os.environ["POSTGRES_PORT"]}/{db_name}"
     )
--- allocations/tests/conftest.py
+++ allocations/tests/conftest.py
@@ -15,28 +15,9 @@ def load_env():
     dotenv.load_dotenv()
 
 
-# @pytest.fixture
-# def make_session():
-#     engine = sqlalchemy.create_engine(config.get_sqlite(), echo=True)
-#     allocations_orm.mapper_registry.metadata.create_all(engine)
-#     allocations_orm.start_mappers()
-
-#     yield sqlalchemy_orm.sessionmaker(bind=engine)
-
-#     allocations_orm.mapper_registry.metadata.drop_all(engine)
-#     allocations_orm.mapper_registry.dispose()
-
-
-# @pytest.fixture
-# def session(make_session):
-#     _session = make_session()
-
-#     yield _session
-
-
 @pytest.fixture
 def make_session():
-    engine = sqlalchemy.create_engine(config.get_postgres(), echo=True)
+    engine = sqlalchemy.create_engine(config.get_postgres(test=True), echo=True)
     allocations_orm.mapper_registry.metadata.create_all(engine)
     allocations_orm.start_mappers()
 
