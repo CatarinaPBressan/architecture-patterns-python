8f4a2a820c122bb7ef9ca5b6abf7d760ef354e57
Catarina Bressan <catarina@catarinapbressan.org>
2026-04-07 09:52:59 -0300

    Adds alembic & initial migration closes #4

---
+++ alembic/env.py
@@ -0,0 +1,80 @@
+from logging.config import fileConfig
+
+import dotenv
+from sqlalchemy import create_engine
+
+from alembic import context
+from allocations import config as allocations_config
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+from allocations.adapters import orm as allocations_orm
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+
+target_metadata = allocations_orm.mapper_registry.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    dotenv.load_dotenv()
+    url = allocations_config.get_postgres()
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    dotenv.load_dotenv()
+    connectable = create_engine(
+        allocations_config.get_postgres(), **allocations_config.get_postgres_engine_kwargs()
+    )
+
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
